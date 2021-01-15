50e5c39ba792a6588652c37985bb360e638031e0
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-15 21:27:05 +0530

    Initial Commit

---
+++ Dashboard App UI/Dashboard_App_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dashboard_App_UIApp.swift
+//  Dashboard App UI
+//
+//  Created by Mahesh Prasad on 15/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Dashboard_App_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
